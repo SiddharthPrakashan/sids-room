7132d19dfcacca05274a01b1eb6fd5c15276bbf6
Siddharth Prakashan <prakashansiddharth@gmail.com>
2020-08-15 13:30:11 +0530

    sidedrawer

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+import { Place } from '../../place.model';
+import { ActivatedRoute } from '@angular/router';
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-place-detail',
@@ -6,10 +10,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./place-detail.page.scss'],
 })
 export class PlaceDetailPage implements OnInit {
+  place: Place;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private navCtrl: NavController,
+    private placeService: PlacesService
+  ) {}
 
   ngOnInit() {
+    this.route.paramMap.subscribe((paramMap) => {
+      if (!paramMap.has('placeId')) {
+        this.navCtrl.navigateBack('/places/tabs/discover');
+        return;
+      }
+      this.place = this.placeService.getPlace(paramMap.get('placeId'));
+    });
   }
 
+  onBookPlace() {
+    this.navCtrl.navigateBack('/places/tabs/discover');
+  }
 }
--- src/app/places/places.service.ts
+++ src/app/places/places.service.ts
@@ -33,5 +33,9 @@ export class PlacesService {
     return [...this._places];
   }
 
+  getPlace(id: string) {
+    return this._places.find((p) => p.id === id);
+  }
+
   constructor() {}
 }
